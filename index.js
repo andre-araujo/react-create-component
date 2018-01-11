e0b5989ab3b69b60cd4b5ceeaa70c952b7c4350c
andre-araujo <andreluis9214@gmail.com>
2018-01-11 11:34:03 -0200

    feat: Create generate component function

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-module.exports = console.log('ok');
+const fs = require('fs');
+const args = process.argv;
+const cwd = process.cwd();
+let [
+    componentName,
+    componentFolder,
+] = [args[2], args[3]];
+
+function generateComponent(template = 'default') {
+    const templateDir = `${__dirname}/templates/${template}`;
+    const templateFiles = fs.readdirSync(templateDir);
+    const dir = cwd + (componentFolder && componentFolder != 'undefined' ? `/${componentFolder}` : '');
+
+    if (!componentName) {
+        process.stdout.write(`
+Your component must have a name.
+        \n`);
+        return;
+    }
+
+    componentName = componentName
+        .replace(/^./, (compFirstLetter) => compFirstLetter.toUpperCase())
+
+    try {
+        fs.mkdirSync(`${dir}/${componentName}`)
+    } catch (e) {
+        if(e.code === 'EEXIST') {
+            process.stdout.write(`
+A component named ${componentName} already exists!
+Try another component name.
+            \n`);
+        }
+    
+        if(e.code === 'ENOENT') {
+            process.stdout.write(`
+Path ${componentFolder} not found at ${cwd}
+            \n`);
+        }
+        
+        return;
+    }
+
+    templateFiles.forEach(templateFile => {
+        const fileName = templateFile.replace(/{{name}}/g, componentName);
+
+        const file = fs.readFileSync(`${templateDir}/${templateFile}`, 'utf8');
+        const fileWithComponentName = file.replace(/{{name}}/g, componentName+1);
+
+        fs.writeFileSync(`${dir}/${componentName}/${fileName}`, fileWithComponentName)
+    })
+    process.stdout.write('DONE!\n');
+}
+
+generateComponent();
